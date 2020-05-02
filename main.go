1b86e07c33aa35e063f27a8a0dd843293949bfe9
Zack Scholl <zack.scholl@gmail.com>
2020-05-02 08:57:31 -0700

    bump 1.1.5

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ var flagCompressed, flagVerbose, flagNoClobber, flagStdout, flagUseTor, flagDoSt
 var flagStripScript, flagStripStyle bool
 var flagList, flagOutfile string
 var flagHeaders arrayFlags
-var Version = "v1.1.5-c968160"
+var Version = "v1.1.5-8a0155d"
 var showTorIP bool
 var spin *spinner.Spinner
 var hpool *httppool.HTTPPool
