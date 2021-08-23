8654560a35fd4fa0b72587dfa2e053679fc94e0f
Zack Scholl <zack.scholl@gmail.com>
2021-08-23 11:03:22 -0700

    bump 1.1.11

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ var flagCompressed, flagVerbose, flagNoClobber, flagStdout, flagUseTor, flagDoSt
 var flagStripScript, flagStripStyle bool
 var flagList, flagOutfile string
 var flagHeaders arrayFlags
-var Version = "v-c676ef4"
+var Version = "v1.1.11-78d01bd"
 var showTorIP bool
 var spin *spinner.Spinner
 var hpool *httppool.HTTPPool
